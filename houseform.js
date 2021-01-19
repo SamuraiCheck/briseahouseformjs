bf7ba4fa895ab8494fb1ca3ccd9ac87707b8db72
SamuraiCheck <63857345+SamuraiCheck@users.noreply.github.com>
2021-01-19 12:38:16 +0200

    Update houseform.js

--- houseform.js
+++ houseform.js
@@ -63,7 +63,7 @@ function homeMore() {
 }
 
 function homeFirstDropdown() {  
-  if (document.getElementById('property-type').value == "single-family-home") {
+  if (document.getElementById('property-type').value == "singlefamily") {
    console.log("show info")
     document.getElementById('property-type').style.cssText = "display: none;"
     document.getElementById('property-information').style.cssText = "display: block;"
